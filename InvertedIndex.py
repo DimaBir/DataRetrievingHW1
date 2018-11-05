17eae371824a4d79fc86cd3ce2049ca470006297
DimaVlas <dvlasenko86@gmail.com>
2018-11-05 11:33:57 +0200

    Upercase for TrecFileParser

--- InvertedIndex.py
+++ InvertedIndex.py
@@ -1,7 +1,7 @@
 from os import listdir
 from os.path import isfile, join
 import pickle
-from trecFileParser import parsetrecfile
+from TrecFileParser import parsetrecfile
 
 class Node(object):
     def __init__(self, data, prev, next):
