5a8f7dc96c6407202481850a5b5857b3e6708727
Dmitry Vlasenko <dvlasenko86@gmail.com>
2018-11-06 00:25:12 +0200

    Updated comments

--- ManualTrecFileParser.py
+++ ManualTrecFileParser.py
@@ -6,47 +6,52 @@ def printDictionary(dictionary):
     for key in dictionary:
         print("DOCNO :", key, "\nTEXT =", dictionary[key], "\n")
 
-
+# Parses trec file, using regex expressions
+# params:
+# - pathtofile: path to trec file 
+# return: 
+# - Dictionary: Key=DOCNO, Value=TEXT 
+# Value as Set of splitted strings from <TEXT><\TEXT> without stop words
 def RegexParseTrecFile(pathtofile):
     # Reading file
     with open(pathtofile, 'r') as f:
         trecFileString = f.read()
 
     # Adding a fake root tag (Not necessary in our parser)
-        trecFileString = '<ROOT>' + trecFileString + '</ROOT>'
+    trecFileString = '<ROOT>' + trecFileString + '</ROOT>'
 
     # Initialize result dictionary
     results = {}
 
-    # Find all documents in file
+    # Find all documents in file, including subtags. 
     documents = re.findall("<DOC>[\s\S]*?<\/DOC>", trecFileString)
 
-    # Iterate through each DOC and gather DOCNO and TEXT
+    # Iterate through each DOC and cut only DOCNO and TEXT tags
     for doc in documents:
-        # Regex Pattern to find all between <DOC></DOC>
+        # Regex Pattern to find content between <DOC></DOC>
         docnoTagRegexResultList = re.findall("<DOCNO>(.*?)<\/DOCNO>", doc)
 
-        # If DOC don`t have DOCNO tag was, don`t continue
+        # If DOC don`t have DOCNO tag, don`t continue
         if not docnoTagRegexResultList:
             continue
 
-        # Transform return value (that is List) to string
+        # Transform returned regex match from List to string
         docno = ''.join(docnoTagRegexResultList)
 
-        # Find all between <TEXT></TEXT>
+        # Extract all text between <TEXT></TEXT>
         textTagRegexResultList = re.findall("<TEXT>(.*?)<\/TEXT>", doc, re.S)
 
-        # If text in DOC was empty , don`t continue
+        # If text in DOC was empty or tag do not exists, don`t continue
         if not textTagRegexResultList:
             continue
 
-        # Transform result list to string
+        # Transform regex match from List to string
         textTagRegexResultListString = ''.join(textTagRegexResultList)
 
-        # Clear end of lines and tabs from string
+        # Remove end of lines and tabs from string
         textTagRegexResultListClear = textTagRegexResultListString.replace('\n', ' ').replace('\r', '')
 
-        # Clear all Stop Words from text
+        # Remove all Stop Words from text
         text = FilterStopWords(textTagRegexResultListClear)
 
         # Add legal elements to result dictionary
