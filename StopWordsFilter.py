fe4116a88c08df489daefe52cd5662a48a1f75e6
Dmitry Vlasenko <dvlasenko86@gmail.com>
2018-11-07 09:59:06 +0200

    Update Comments

--- StopWordsFilter.py
+++ StopWordsFilter.py
@@ -1,14 +1,21 @@
+# The collection of English Stop Words
 stopWordsEnglish = ['ourselves', 'hers', 'between', 'yourself', 'but', 'again', 'there', 'about', 'once', 'during', 'out', 'very', 'having', 'with', 'they', 'own', 'an', 'be', 'some', 'for', 'do', 'its', 'yours', 'such', 'into', 'of', 'most', 'itself', 'other', 'off', 'is', 's', 'am', 'or', 'who', 'as', 'from', 'him', 'each', 'the', 'themselves', 'until', 'below', 'are', 'we', 'these', 'your', 'his', 'through', 'don', 'nor', 'me', 'were', 'her', 'more', 'himself', 'this', 'down', 'should', 'our', 'their', 'while', 'above', 'both', 'up', 'to', 'ours', 'had', 'she', 'all', 'no', 'when', 'at', 'any', 'before', 'them', 'same', 'and', 'been', 'have', 'in', 'will', 'on', 'does', 'yourselves', 'then', 'that', 'because', 'what', 'over', 'why', 'so', 'can', 'did', 'not', 'now', 'under', 'he', 'you', 'herself', 'has', 'just', 'where', 'too', 'only', 'myself', 'which', 'those', 'i', 'after', 'few', 'whom', 't', 'being', 'if', 'theirs', 'my', 'against', 'a', 'by', 'doing', 'it', 'how', 'further', 'was', 'here', 'than']
 
-
+# Removes all stop words from text
+# params: text - contains text as string that must be filtered
+# returns: set of strings without stop words
 def FilterStopWords(text):
 
+    # Transform collection of stop words to set
     stop_words = set(stopWordsEnglish)
-
+    
+    # Transform text to set of strings
     word_tokens = text.split()
 
+    # Init result set
     filtered_sentence = []
 
+    # For esch word from text check if it is a stop word, clear stopr words
     for w in word_tokens:
         if w not in stop_words:
             filtered_sentence.append(w)
